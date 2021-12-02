cd8ec7f884821cd2bda9891022a60e18bfab4d7f
Neil Lambert <nlambert@doximity.com>
2021-12-01 22:38:12 -0800

    refactor 2021.day-02: favor inline functions

--- src/calendar/2021/day_02.clj
+++ src/calendar/2021/day_02.clj
@@ -2,27 +2,18 @@
   (:require [advent.core :as a]))
 
 (def input (a/each-line "2021/day_02.txt"))
-;; part one
+;; helpers
+(defn decrease-by [x] (fn [y] (- y (Integer. x))))
+(defn increase-by [x] (fn [y] (+ y (Integer. x))))
 (defn answer [sub] (* (sub :horizontal) (sub :depth)))
-(defn forward [sub amt] (update sub :horizontal #(+ % amt)))
-(defn up      [sub amt] (update sub :depth  #(- % amt)))
-(defn down    [sub amt] (update sub :depth  #(+ % amt)))
-;; part two
-(defn aim-up      [sub amt] (update sub :aim  #(- % amt)))
-(defn aim-down    [sub amt] (update sub :aim  #(+ % amt)))
-(defn aim-forward
-  [sub amt]
-  (as-> sub $
-    (forward $ amt)
-    (down $ (* amt ($ :aim)))))
 
 (defn part-one [instructions]
   (reduce
    (fn [sub instruction]
      (condp re-matches instruction
-       #"forward (\d+)" :>> (fn [[_ amt]] (forward sub (Integer. amt)))
-       #"up (\d+)"      :>> (fn [[_ amt]] (up sub (Integer. amt)))
-       #"down (\d+)"    :>> (fn [[_ amt]] (down sub (Integer. amt)))))
+       #"up (\d+)"      :>> (fn [[_ amt]] (update sub :depth (decrease-by amt)))
+       #"down (\d+)"    :>> (fn [[_ amt]] (update sub :depth (increase-by amt)))
+       #"forward (\d+)" :>> (fn [[_ amt]] (update sub :horizontal (increase-by amt)))))
    {:horizontal 0 :depth 0}
    instructions))
 
@@ -30,11 +21,14 @@
   (reduce
    (fn [sub instruction]
      (condp re-matches instruction
-       #"forward (\d+)" :>> (fn [[_ amt]] (aim-forward sub (Integer. amt)))
-       #"up (\d+)"      :>> (fn [[_ amt]] (aim-up sub (Integer. amt)))
-       #"down (\d+)"    :>> (fn [[_ amt]] (aim-down sub (Integer. amt)))))
+       #"up (\d+)"      :>> (fn [[_ amt]] (update sub :aim (decrease-by amt)))
+       #"down (\d+)"    :>> (fn [[_ amt]] (update sub :aim (increase-by amt)))
+       #"forward (\d+)" :>> (fn [[_ amt]] (-> sub
+                                              (update :horizontal (increase-by amt))
+                                              (update :depth (increase-by (* (sub :aim)
+                                                                             (Integer. amt))))))))
    {:horizontal 0 :depth 0 :aim 0}
    instructions))
 
-(-> (part-one input) answer) ;; {:horizontal 2052, :depth 1032} => 2117664
-(-> (part-two input) answer) ;; {:horizontal 2052, :depth 1010437, :aim 1032} => 2073416724
+(= (answer (part-one input)) 2117664) ;; {:horizontal 2052, :depth 1032}
+(= (answer (part-two input)) 2073416724) ;; {:horizontal 2052, :depth 1010437, :aim 1032}
