fdb4612dd59c890ce37b3a1f5950bbc074a86f6d
Neil Lambert <nlambert@doximity.com>
2021-12-08 11:44:13 -0800

    teach elves new tricks

--- src/advent/core.clj
+++ src/advent/core.clj
@@ -15,5 +15,16 @@
     (string/split $input matcher)
     (map mapfn $input)))
 
+(defn abs-range 
+  "Generates a range between two points on a line regardless of the order in which the points are given"
+  [a b]
+  (cond
+    (< a b) (range a (inc b))    ;; incrementing range
+    (> a b) (range a (dec b) -1) ;; decrementing range
+    (= a b) (repeat a)))
+
+(defn difference [a b] (Math/abs (- a b)))
+(defn sum [coll] (apply + coll))
+
 (defn -main [& _args]
   (println "Hello"))
